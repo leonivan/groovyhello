bb540d8fb18dc1fc2a00dbd3ecdc007c37830933
vlado <vlado@vmarquezp-dh01.local>
2018-04-24 13:41:40 -0400

    my fisrt from local

---
+++ src/main/groovy/com/vk/hello.groovy
@@ -0,0 +1,10 @@
+//File: src/main/groovy/com/vk/hello.groovy
+package com.vk
+
+x = /Vlad, you Rocks!!!/
+firstname = "Vladimir"
+lastname = "Marquez"
+println "Hello ${firstname[0]}.$lastname"
+println "Really $x"
+
+
